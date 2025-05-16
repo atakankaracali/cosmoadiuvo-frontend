a87c9a6b66eca1630f68858b591c72d67c926e0d
Atakan Karaçalı <atakan.karacali97@gmail.com>
2025-05-16 11:20:02 +0300

    Adding-New-Pages

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "../styles/notfound.css";
+import "../styles/nonfound.css";
 import { useTranslation } from "react-i18next";
 import StarsOverlay from "../components/StarsOverlay";
 
